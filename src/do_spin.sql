258f614197670f6ddd0dbe801c2b093c580938b9
apande <ani.pande1995@gmail.com>
2022-11-25 18:18:03 -0500

    SQLite3

--- src/do_spin.sql
+++ src/do_spin.sql
@@ -1,27 +1,34 @@
-CREATE USER 'admin'@'localhost' IDENTIFIED BY 'admin';
+-- CREATE USER 'admin'@'localhost' IDENTIFIED BY 'admin';
 
-CREATE DATABASE userdb;
+-- CREATE DATABASE userdb;
 
-GRANT ALL PRIVILEGES ON userdb.* TO 'admin'@'localhost';
+-- GRANT ALL PRIVILEGES ON userdb.* TO 'admin'@'localhost';
 
-USE userdb;
+-- USE userdb;
 
-CREATE TABLE user (
-    id INT NOT NULL AUTO_INCREMENT PRIMARY KEY,
-    uname VARCHAR(255) NOT NULL,
-    password VARCHoAR(255) NOT NULL
+-- CREATE TABLE user (
+--     id INT NOT NULL AUTO_INCREMENT PRIMARY KEY,
+--     uname VARCHAR(255) NOT NULL,
+--     password VARCHoAR(255) NOT NULL
+-- );
+
+-- INSERT INTO user (uname, password) VALUES ('admin1','admin1');
+-- INSERT INTO user (uname, password) VALUES ('admin2','admin2');
+-- INSERT INTO user (uname, password) VALUES ('admin3','admin3');
+-- INSERT INTO user (uname, password) VALUES ('admin4','admin4');
+-- INSERT INTO user (uname, password) VALUES ('admin5','admin5');
+-- INSERT INTO user (uname, password) VALUES ('admin6','admin6');
+-- INSERT INTO user (uname, password) VALUES ('admin7','admin7');
+-- INSERT INTO user (uname, password) VALUES ('admin8','admin8');
+-- INSERT INTO user (uname, password) VALUES ('admin9','admin9');
+-- INSERT INTO user (uname, password) VALUES ('admin10','admin10');
+-- INSERT INTO user (uname, password) VALUES ('admin11','admin11');
+-- INSERT INTO user (uname, password) VALUES ('admin12','admin12');
+-- INSERT INTO user (uname, password) VALUES ('admin13','admin13');
+
+DROP TABLE IF EXISTS reqs;
+
+CREATE TABLE reqs (
+    id INTEGER PRIMARY KEY AUTOINCREMENT,
+    req_rand TEXT DEFAULT "TEST"
 );
-
-INSERT INTO user (uname, password) VALUES ('admin1','admin1');
-INSERT INTO user (uname, password) VALUES ('admin2','admin2');
-INSERT INTO user (uname, password) VALUES ('admin3','admin3');
-INSERT INTO user (uname, password) VALUES ('admin4','admin4');
-INSERT INTO user (uname, password) VALUES ('admin5','admin5');
-INSERT INTO user (uname, password) VALUES ('admin6','admin6');
-INSERT INTO user (uname, password) VALUES ('admin7','admin7');
-INSERT INTO user (uname, password) VALUES ('admin8','admin8');
-INSERT INTO user (uname, password) VALUES ('admin9','admin9');
-INSERT INTO user (uname, password) VALUES ('admin10','admin10');
-INSERT INTO user (uname, password) VALUES ('admin11','admin11');
-INSERT INTO user (uname, password) VALUES ('admin12','admin12');
-INSERT INTO user (uname, password) VALUES ('admin13','admin13');
--- src/server.py
+++ src/server.py
@@ -2,33 +2,66 @@
 from flask import Flask
 import time
 import random
-# from flask_mysqldb import MySQL
+import sqlite3
+
 
 app = Flask(__name__)
-# app.config["MYSQL_USER"] = os.environ.get("MYSQL_USER")
-# app.config["MYSQL_PASSWORD"] = os.environ.get("MYSQL_PASSWORD")
-# app.config["MYSQL_DB"] = os.environ.get("MYSQL_DB")
 
-# mysql = MySQL(app)
+# connect to sqlite3
+def db_connect():
+    conn = sqlite3.connect("database.db")
+    conn.row_factory = sqlite3.Row
+    return conn
 
 
 @app.route("/", methods=["GET", "POST"])
 def stress():
     """_summary_: stress the cpu. It will run a loop for 1 second."""
     random_number = random.randint(2, 100)
-    loop_end_time = time.time() + .2
+    loop_end_time = time.time() + 0.2
     while time.time() < loop_end_time:
         random_number = random_number * random_number
     return "OK", 200
 
 
-# @app.route("/db", methods=["GET", "POST"])
-# def db():
-#     """Read from DB and return."""
-#     cur = mysql.connection.cursor()
-#     cur.execute("SELECT * FROM user;")
-#     data = cur.fetchall()
-#     return str(data), 200
+@app.route("/db", methods=["GET", "POST"])
+def db():
+    """Fill random data to the database."""
+    random_int = random.randint(1, 100)
+    try:
+        conn = db_connect()
+        conn.execute("INSERT INTO reqs ( req_rand) VALUES (?)", (random_int,))
+        conn.commit()
+        conn.close()
+    except Exception as e:
+        return "Error: " + str(e), 500
+    return "OK", 200
+
+
+@app.route("/db_init", methods=["GET", "POST"])
+def db_init():
+    """Create the database."""
+    try:
+        connection = sqlite3.connect("database.db")
+        with open("do_spin.sql") as f:
+            connection.executescript(f.read())
+    except Exception as e:
+        return "Error: " + str(e), 500
+    return "OK", 200
+
+
+@app.route("/db_fetch", methods=["GET", "POST"])
+def db_fetch():
+    """Fetch data from the database."""
+    try:
+        conn = db_connect()
+        cursor = conn.execute("SELECT * FROM reqs")
+        rows = cursor.fetchall()
+        count = len(rows)
+        conn.close()
+    except Exception as e:
+        return "Error: " + str(e), 500
+    return str(count), 200
 
 
 if __name__ == "__main__":
